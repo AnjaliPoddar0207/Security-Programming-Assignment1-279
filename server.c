a56660ed05b1fd905dd9f69d13d8464113a3d6b3
Anjali <anjali.poddar0207@gmail.com>
2021-11-11 13:52:09 -0800

    added function definitions for the client program

--- server.c
+++ server.c
@@ -2,31 +2,31 @@
 #include <unistd.h> 
 #include <stdio.h> 
 #include <sys/socket.h> 
-#include <sys/types.h>
 #include <stdlib.h> 
 #include <netinet/in.h> 
 #include <string.h> 
-#include <pwd.h>
+#include <sys/wait.h>
+#define RED(string) "\x1b[31m" string "\x1b[0m"
+#define BLUE(string) "\x1b[34m" string "\x1b[0m"
 #define PORT 8080 
 int main(int argc, char const *argv[]) 
 { 
     int server_fd, new_socket, valread; 
     struct sockaddr_in address; 
     int opt = 1; 
-    int addrlen = sizeof(address); 
+    int addrlen = sizeof(address);
+    int val; 
     char buffer[1024] = {0}; 
+    
     char *hello = "Hello from server"; 
-    pid_t child_pid, f_pid;
-    struct passwd* passwd_ptr;
-    int status;
-
-    printf("Log: Parent Process: Current pid = %d\n",getpid());
-    printf("Log: Parent Process: Current uid = %ld\n",(long) getuid());   
-
+    pid_t parent_pid;   
+    pid_t current_pid; 
+    current_pid=getpid();  
+ 
     // Creating socket file descriptor 
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
     { 
-        perror("socket failed"); 
+        perror(RED("socket failed")); 
         exit(EXIT_FAILURE); 
     } 
        
@@ -34,18 +34,20 @@ int main(int argc, char const *argv[])
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
                                                   &opt, sizeof(opt))) 
     { 
-        perror("setsockopt"); 
+        perror(RED("setsockopt")); 
         exit(EXIT_FAILURE); 
     } 
     address.sin_family = AF_INET; 
     address.sin_addr.s_addr = INADDR_ANY; 
     address.sin_port = htons( PORT ); 
+    
+    
        
     // Forcefully attaching socket to the port 8080 
     if (bind(server_fd, (struct sockaddr *)&address,  
                                  sizeof(address))<0) 
     { 
-        perror("bind failed"); 
+        perror(RED("bind failed")); 
         exit(EXIT_FAILURE); 
     } 
     if (listen(server_fd, 3) < 0) 
@@ -56,48 +58,47 @@ int main(int argc, char const *argv[])
     if ((new_socket = accept(server_fd, (struct sockaddr *)&address,  
                        (socklen_t*)&addrlen))<0) 
     { 
-        perror("accept"); 
+        perror(RED("accept")); 
         exit(EXIT_FAILURE); 
+    } 
+	printf(BLUE("*start*\n"));
+	/*Solution for Assignment-1
+	removing the privilege for the read and send functions of the server
+	by creating a child process
+	run the server as root process..using sudo.
+	*/
+    current_pid=fork(); // creates a child process
+    if(current_pid == 0)
+    {
+	printf("Child created...reading from client\n");
+	printf("child id: %d\n",current_pid);
+	val = setuid(65534); /*user - nobody(uid - 65534), current_user(uid -1000)
+	use id command to get the userIds
+	*/
+	printf("val: %d\tcurrent_id: %d\n",val,getuid());
+	if(val ==-1){
+	printf(RED("Error dropping privilege\n"));
+	return 0;
+	}
+    	valread = read( new_socket , buffer, 1024); 
+    	printf("%s\n",buffer ); 
+    	send(new_socket , hello , strlen(hello) , 0 ); 
+    	printf("Hello message sent\n"); 
+	printf(RED("IN CHILD ->")BLUE("current_user_id: %d\n"),getuid());
     }
-
-    // Forking
-    child_pid = fork();                                                                                                                                              
-    if( child_pid == 0 ){
-        // fork returns 0 inside child process
-        printf("Log: Child Process: forked child_pid = %d\n", child_pid);
-        printf("Log: Child Process: Current pid = %d\n",getpid());
-        printf("Log: Child Process: Current uid = %ld\n", (long) getuid()); 
-        //Changing user from root to "nobody"
-        passwd_ptr = getpwnam("nobody");
-        printf("Log: Child Process: UID of nobody=%ld\n",(long) passwd_ptr->pw_uid);
-        if (setuid(passwd_ptr->pw_uid) != 0)   
-            perror("Log: Child Process: setuid() error");
-        else
-            printf("Log: Child Process: UID after setuid() = %ld\n",(long) getuid());
-
-        valread = read( new_socket , buffer, 1024); 
-        printf("%s\n",buffer ); 
-        send(new_socket , hello , strlen(hello) , 0 ); 
-        printf("Hello message sent\n");
-
+    else if(current_pid > 0)
+    {
+	wait(NULL);
+	printf("control returns to parent...\n");
+	
     }
-    else if (child_pid > 0){
-        //fork return PID of child process inside parent
-        //Waiting for child process to complete
-        printf("Log: Parent Process: Waiting for child process to complete\n");
-        if((f_pid = wait(&status)) < 0){
-            perror("Log: Parent Process: Error in wait");
-            _exit(1);
-        }
-        printf("Log: Parent Process: Forked child pid: %d\n", child_pid); 
-        printf("Log: Parent Process: Current pid = %d\n",getpid());
-        printf("Log: Parent Process: Current uid = %ld\n",(long) getuid());   
-        printf("Log: Parent Process: Completed\n");
-
+    else
+    {
+	perror(RED("fork failed\n"));
+	_exit(2);
     }
-    else{
-        perror("Error! fork() failed");
-        _exit(2);                                                                                                                                          
-    }
+    //printf(" IN PARENT -> current_user_id: %d\n",getuid());
+    printf(RED("DEBUG /ppid returns:"BLUE("%d\n")),parent_pid);
+    printf(BLUE("*END*\n"));
     return 0; 
 } 
